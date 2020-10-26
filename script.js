47139678221f8ad39a9331fe328b2b136aff4152
areilly1 <58097168+areilly1@users.noreply.github.com>
2020-10-26 10:45:07 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".in").click(function() {
+    $(".gif1").fadeIn().delay(1500).fadeOut();
+    $(".title").fadeOut().delay(1500).fadeIn();
+    $(".pic").fadeOut().delay(1500).fadeIn();
+    $(".other").fadeOut().delay(1500).fadeIn();
+    $(".out").fadeOut().delay(1500).fadeIn();
+    $(".in").fadeOut().delay(1500).fadeIn();
+    $(".font1").fadeOut().delay(1500).fadeIn();
+    $(".long").fadeOut().delay(1500).fadeIn();
+});
+
+$(".out").click(function() {
+    $(".gif2").fadeIn().delay(2000).fadeOut();
+    $(".title").fadeOut().delay(2000).fadeIn();
+    $(".pic").fadeOut().delay(2000).fadeIn();
+    $(".other").fadeOut().delay(2000).fadeIn();
+    $(".out").fadeOut().delay(2000).fadeIn();
+    $(".in").fadeOut().delay(2000).fadeIn();
+    $(".font1").fadeOut().delay(2000).fadeIn();
+    $(".long").fadeOut().delay(2000).fadeIn();
+});
